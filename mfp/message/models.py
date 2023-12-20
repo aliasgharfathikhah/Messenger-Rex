8f56a7ac918c6a088b10f33f82cef54dcc5d4515
ali fathi <ali.fathi.13121110@gmail.com>
2023-12-20 17:08:43 +0330

    add model message

--- mfp/message/models.py
+++ mfp/message/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Model for message
+class Message(models.Model):
+    text = models.TextField()
+    for_user = models.ForeignKey(User, on_delete=models.CASCADE)
